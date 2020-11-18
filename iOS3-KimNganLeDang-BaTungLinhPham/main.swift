49bb8993afa03cdf7e6ff7b194ce3ece51bf3ee2
Kim Ngan <kimngan.ledang@gmail.com>
2020-11-18 16:32:52 +0100

    Main() add read() func

--- iOS3-KimNganLeDang-BaTungLinhPham/main.swift
+++ iOS3-KimNganLeDang-BaTungLinhPham/main.swift
@@ -7,7 +7,30 @@
 
 import Foundation
 
-func readOrder() -> String{
-    let order = readLine()!
-    return order
+//func readOrder() -> String{
+//    let order = readLine()!
+//    return order
+//}
+//
+
+func read(withPrompt: String) -> Any? {
+    print(withPrompt,terminator: "")
+    
+    if let input = readLine() {
+        if let num = Int(input) {//if input is integer
+            return num
+        }
+        return input
+    }
+    return nil
 }
+
+///* Test print */
+//func printTest () {
+//    let name = read(withPrompt: "Input your name: ")
+//    let age = read(withPrompt: "Input your age: ")
+//    print("Name: ", name!)
+//    print("Age: ", age!)
+//}
+//
+//printTest()
